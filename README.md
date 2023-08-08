5b74cae2f02771b567c634d933ebd78a0fb13918
Rameez Khan <rameezkhan.sa@gmail.com>
2023-08-08 11:14:32 +0200

    Add jump script

---
+++ README.md
@@ -0,0 +1,17 @@
+# jump
+
+> Establish a tunnel to a remote host:port via SSM
+
+## Prerequisites
+* [Nix](https://nixos.org/)
+
+## Usage
+
+Copy the example config.
+```shell
+cp config.example.yaml config.yaml
+```
+Then establish a session.
+```shell
+./jump.py <name-of-jump>
+```
---
+++ jump.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -i python3 -p python3Packages.typer python3Packages.pyyaml awscli2 ssm-session-manager-plugin
+
+import json
+import subprocess
+from dataclasses import dataclass
+
+import typer
+import yaml
+
+app = typer.Typer()
+
+
+@dataclass(kw_only=True)
+class Jump:
+    name: str  # Name of jump
+    target_instance_name: str  # Name of the target to lookup
+    remote_host: str  # Remote host to jump to
+    remote_port: int  # Port to jump to on remote host
+    local_port: int  # Local port to bind to
+    aws_profile: str  # AWS profile to use for AWS cli commands
+
+
+def parse_jumps_from_config() -> dict[str, Jump]:
+    try:
+        with open("config.yaml", "r") as f:
+            raw = yaml.safe_load(f)
+            jumps = [Jump(**j) for j in raw["jumps"]]
+            return {j.name: j for j in jumps}
+    except Exception as e:
+        typer.secho(
+            f"Failed to parse config.yaml. Please have a look at config.example.yaml for syntax. The error: {e}",
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(code=1)
+
+
+def lookup_instance_id(instance_name: str, aws_profile: str) -> str:
+    typer.secho(
+        f"Looking up the instance ID of {instance_name}", fg=typer.colors.MAGENTA
+    )
+    try:
+        cmd = f'aws --profile {aws_profile} ec2 describe-instances --filters Name=tag-value,Values={instance_name} --query "Reservations[*].Instances[*].[InstanceId]" --output text'
+        instance_id = subprocess.check_output(cmd, shell=True).decode().strip()
+    except subprocess.CalledProcessError:
+        typer.secho(
+            f"Failed to lookup the instance ID of {instance_name}", fg=typer.colors.RED
+        )
+        raise typer.Exit(code=1)
+    return instance_id
+
+
+def start_ssm_session(
+    target_instance_id: str,
+    remote_host: str,
+    remote_port: int,
+    local_port: int,
+    aws_profile: str,
+) -> None:
+    typer.secho(f"Establishing an SSM session", fg=typer.colors.MAGENTA)
+
+    parameters = {
+        "host": [str(remote_host)],
+        "portNumber": [str(remote_port)],
+        "localPortNumber": [str(local_port)],
+    }
+    cmd = f"aws --profile {aws_profile} "
+    cmd += f"ssm start-session --target {target_instance_id} "
+    cmd += f"--document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "
+    cmd += f"'{json.dumps(parameters)}'"
+
+    try:
+        subprocess.run(cmd, shell=True)
+    except subprocess.CalledProcessError:
+        typer.secho(f"Failed to establish an SSM session", fg=typer.colors.RED)
+        raise typer.Exit(code=1)
+
+
+@app.command(help="Establish a tunnel to a remote host using SSM")
+def jump(
+    name: str = typer.Argument(..., help="Name of the jump (specified in config.yaml)")
+):
+    jumps = parse_jumps_from_config()
+    target = jumps.get(name, None)
+    if target is None:
+        typer.secho(
+            f"Jump {name} not specified in config.yaml",
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(code=1)
+
+    target_instance_id = lookup_instance_id(
+        target.target_instance_name, target.aws_profile
+    )
+    start_ssm_session(
+        target_instance_id,
+        target.remote_host,
+        target.remote_port,
+        target.local_port,
+        target.aws_profile,
+    )
+
+
+if __name__ == "__main__":
+    app()
